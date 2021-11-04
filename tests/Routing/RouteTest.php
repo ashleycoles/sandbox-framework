1a2af2e989ec24872c06ce6c64eaef25e53ca928
Ashley Coles <ashley@acoles.co.uk>
2021-11-04 18:12:02 +0000

    Starting tests

---
+++ tests/Routing/RouteTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Sandbox\Routing\Route;
+
+class RouteTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $result = new Route('/', 'GET', 'test');
+        $this->assertInstanceOf(Route::class, $result);
+    }
+    public function testConstructMalforded()
+    {
+        $this->expectException(TypeError::class);
+        new Route(['/'], 12, false);
+    }
+    public function testGetURI()
+    {
+        $sut = new Route('/', 'GET', 'test');
+        $result = $sut->getURI();
+        $this->assertEquals($result, '/');
+    }
+}
