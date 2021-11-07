58ae2ee3ce7802c3b5e00b941aa4cd7d9746fe14
Ashley Coles <ashley@acoles.co.uk>
2021-11-07 15:16:23 +0000

    More container tests

--- tests/Container/ContainerTest.php
+++ tests/Container/ContainerTest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use Sandbox\Exceptions\ContainerException;
+use Sandbox\Factories\Request\RequestCreatorFactory;
 
 
 class ContainerTest extends TestCase
@@ -14,7 +16,43 @@ class ContainerTest extends TestCase
         $builtProperty = $containerReflection->getProperty('built');
         $builtProperty->setAccessible(true);
         $builtProperty->setValue($container, true);
-        $this->expectException(\Sandbox\Exceptions\ContainerException::class);
+        $this->expectException(ContainerException::class);
         $container->build();
+    }
+
+    public function testContainerAddWhenAlreadyBuilt()
+    {
+        $container = new Sandbox\Container\Container();
+        $containerReflection = new \ReflectionClass($container);
+        $builtProperty = $containerReflection->getProperty('built');
+        $builtProperty->setAccessible(true);
+        $builtProperty->setValue($container, true);
+        $mockFactory = $this->createMock(RequestCreatorFactory::class);
+        $this->expectException(ContainerException::class);
+        $container->add('test', $mockFactory);
+    }
+
+    public function testContainerAddWhenAlreadyAdded()
+    {
+        $container = new Sandbox\Container\Container();
+        $mockFactory = $this->createMock(RequestCreatorFactory::class);
+        $container->add('test', $mockFactory);
+        $this->expectException(ContainerException::class);
+        $container->add('test', $mockFactory);
+
+    }
+
+    public function testContainerGetWhenNotBuilt()
+    {
+        $container = new Sandbox\Container\Container();
+        $this->expectException(ContainerException::class);
+        $container->get('test');
+    }
+
+    public function testContainerGetWhenNotAdded()
+    {
+        $container = new Sandbox\Container\Container();
+        $this->expectException(ContainerException::class);
+        $container->get('test');
     }
 }
