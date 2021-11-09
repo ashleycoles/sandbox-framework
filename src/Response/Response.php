bfe926bd65bf91a44977e2654ad966da903ae8e6
Ashley Coles <ashley@acoles.co.uk>
2021-11-09 10:57:05 +0000

    Checking status code is valid

--- src/Response/Response.php
+++ src/Response/Response.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sandbox\Response;
 
+use Sandbox\Exceptions\ResponseException;
 use Sandbox\Interfaces\ResponseInterface;
 
 class Response implements ResponseInterface
@@ -55,12 +56,25 @@ class Response implements ResponseInterface
     }
 
     /**
-     * @param  int $status
+     * @param int $status
      * @return ResponseInterface
+     * @throws ResponseException
      */
     public function setStatus(int $status = 200): ResponseInterface
     {
+        $this->assertStatusRange($status);
         http_response_code($status);
         return $this;
+    }
+
+    /**
+     * @param int $status
+     * @throws ResponseException
+     */
+    protected function assertStatusRange(int $status): void
+    {
+        if ($status < 100 || $status >= 600) {
+            throw new ResponseException('Status code must be an integer between 1xx and 5xx');
+        }
     }
 }
