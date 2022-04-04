26fb93d5f277c513a446e6cca6c5e91bd9af3300
Ashley Coles <ashley@acoles.co.uk>
2022-04-04 11:13:35 +0100

    Adding res tests

---
+++ tests/Response/ResponseTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Request;
+
+use PHPUnit\Framework\TestCase;
+use Sandbox\Exceptions\ResponseException;
+use Sandbox\Response\Response;
+
+
+class ResponseTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $result = new Response();
+        $this->assertInstanceOf(Response::class, $result);
+    }
+
+    public function testSetStatus()
+    {
+        $sut = new Response();
+        $result = $sut->setStatus(404);
+        $this->assertInstanceOf(Response::class, $result);
+        // Check the status code was set correctly
+        $this->assertEquals(http_response_code(), 404);
+    }
+
+    public function testSetStatusInvalid()
+    {
+        $sut = new Response();
+        $this->expectException(ResponseException::class);
+        $sut->setStatus(9000);
+    }
+
+    public function testSetContent()
+    {
+        $sut = new Response();
+        $result = $sut->setContent('testing');
+        $this->assertInstanceOf(Response::class, $result);
+        $this->assertEquals('testing', $sut->getContent());
+    }
+
+    public function testSetHeader()
+    {
+        $sut = new Response();
+        $result = $sut->setHeader('Content-Type: application/json');
+        $this->assertInstanceOf(Response::class, $result);
+    }
+}
